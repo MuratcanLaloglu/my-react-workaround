69aae6b1482d14c354ca2f7f9585d1ee3ae70d45
Muratcan Laloğlu <quaristr@gmail.com>
2023-03-11 23:02:54 +0300

    5
    5

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -40,7 +40,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const ExpenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -84,6 +84,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
